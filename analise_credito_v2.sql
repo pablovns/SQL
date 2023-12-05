4afadb4422049d03b7f792146d06ab0da46f5115
pablovns <viniciuspablo962@gmail.com>
2023-12-05 13:43:00 -0300

    Otimizações analise_credito_v2.sql

--- analise_credito_v2.sql
+++ analise_credito_v2.sql
@@ -2,19 +2,18 @@ SELECT
     G.CONTAC,
     C.CONTA,
     C.MATRICULA,
-    REPLACE(REPLACE(C.CPF, '.', ''), '-', '') || REPLACE(
-        REPLACE(REPLACE(C.CGC, '.', ''), '-', ''),
-        '/',
-        ''
-    ) AS CPF_CNPJ,
+    COALESCE(
+        REPLACE(REPLACE(C.CPF, '.', ''), '-', ''), 
+        REPLACE(REPLACE(REPLACE(C.CGC, '.', ''), '-', ''), '/', '') 
+    ) CPF_CNPJ,
     C.NOME,
     E.APELIDO AS AGENTE,
-    C.COD_AGENCIA,
-    AG.NOME AS NOMEAGENCIA,
+    AG.COD_AGENCIA UNID,
+    AG.NOME AS UNIDADE,
     I.COD_PACOTE,
     I.DESCRICAO AS PACOTE,
     C.DEMISSAO,
-    AZ.DT_ABERT AS DT_ABERT_CC,
+    H.DT_ABERT DT_ABERT_CC,
     CASE
         C.NIVEL
         WHEN 1 THEN 'AA'
@@ -30,24 +29,19 @@ SELECT
     END AS NIVEL,
     C.ADM_COOP,
     C.SITUACAO,
-    C.CGC,
     C.FISICA,
-    D.NOME AS CIDADE,
+    D.NOME CIDADE,
     C.MOTIVO_DEMISSAO,
-    C.NASCIMENTO AS DT_NASC,
+    C.NASCIMENTO DT_NASC,
     FACMUTUO.ANOSENTREDATAS(C.NASCIMENTO, SYSDATE) AS IDADE,
     CU.PROFISSAO,
     CU.SALARIO,
-    C.VRFATURAMENTO,
-    AG.COD_AGENCIA AS UNID,
-    Y.SD AS SALDO_CAPITAL,
+    C.VRFATURAMENTO FATURAMENTO,
+    Y.SD SALDO_CAPITAL,
     K.VLR_LIMITE LIMITE,
     FACMUTUO.FACCOR_FUNCTIONS.LIMITE_CHESP(F.CONTAC, SYSDATE) AS LIM_CHESP,
     M.PATRIMONIO,
     N.CHDEV,
-
-    P.ID_GPSOL,
-
     P.DESCR_GRUPO,
     R.RISCO,
     T.DEBITO,
@@ -56,37 +50,17 @@ SELECT
     C.CPFCONJUGE
 FROM
     FACMUTUO.C_CAD C
-    JOIN FACMUTUO.C_CADUNI CU ON CU.CONTA = C.CONTA
-    JOIN FACMUTUO.C_CIDADE D ON C.COD_CID = D.COD_CID
-    JOIN FACMUTUO.C_SETOR S ON CU.COD_SET = S.COD_SET
-    JOIN FACMUTUO.C_BAIRRO B ON C.COD_BAI = B.COD_BAI
+    LEFT JOIN FACMUTUO.C_CADUNI CU ON CU.CONTA = C.CONTA
+    LEFT JOIN FACMUTUO.C_CIDADE D ON C.COD_CID = D.COD_CID
     LEFT JOIN FACMUTUO.C_CAD E ON C.AGENTE = E.CONTA
-    JOIN FACMUTUO.CC_AGENCIA AG ON AG.COD_AGENCIA = C.COD_AGENCIA
-    JOIN FACMUTUO.CC_CADASSOC F ON C.CONTA = F.CONTA
-    AND F.TITULAR = 'T'
+    LEFT JOIN FACMUTUO.CC_AGENCIA AG ON AG.COD_AGENCIA = C.COD_AGENCIA
+    LEFT JOIN FACMUTUO.CC_CADASSOC F ON C.CONTA = F.CONTA AND F.TITULAR = 'T'
     LEFT JOIN FACMUTUO.CC_CONTA G ON F.CONTAC = G.CONTAC
-    JOIN FACMUTUO.CC_CAD H ON F.CONTAC = H.CONTAC
-    JOIN FACMUTUO.CC_PACOTE I ON H.COD_PACOTE = I.COD_PACOTE
-    LEFT JOIN FACMUTUO.E_GPSOL P ON C.CONTA = P.CONTA
-    JOIN (
-        SELECT
-            AZ.CONTAC,
-            MIN(AZ.DT_ABERT) AS DT_ABERT
-        FROM
-            FACMUTUO.CC_CAD AZ
-            JOIN FACMUTUO.CC_CADASSOC BZ ON AZ.CONTAC = BZ.CONTAC
-        WHERE
-            BZ.TITULAR = 'T'
-        GROUP BY
-            AZ.CONTAC
-    ) AZ ON F.CONTAC = AZ.CONTAC
-    LEFT JOIN (
-        SELECT
-            Y.CONTA,
-            Y.SD
-        FROM
-            FACMUTUO.A_SD Y
-    ) Y ON Y.CONTA = C.CONTA
+    LEFT JOIN FACMUTUO.CC_CAD H ON F.CONTAC = H.CONTAC
+    LEFT JOIN FACMUTUO.CC_PACOTE I ON H.COD_PACOTE = I.COD_PACOTE
+    LEFT JOIN FACMUTUO.E_ASSOC_GPSOL O ON C.CONTA = O.CONTA
+    LEFT JOIN FACMUTUO.E_GPSOL P ON O.ID_GPSOL = P.ID_GPSOL
+    LEFT JOIN FACMUTUO.A_SD Y ON C.CONTA = Y.CONTA 
     LEFT JOIN (
         SELECT
             K.CONTAC,
@@ -188,16 +162,13 @@ FROM
             COUNT(*) AS CHDEV
         FROM
             FACMUTUO.CC_CHEQUEC A
-            JOIN FACMUTUO.CC_TALAO B ON A.COD_TALAO = B.COD_TALAO
-            JOIN FACMUTUO.CC_CADASSOC D ON D.CONTAC = B.CONTAC
-            JOIN FACMUTUO.C_CAD E ON E.CONTA = D.CONTA
-            JOIN FACMUTUO.CC_AGENCIA C ON E.COD_AGENCIA = C.COD_AGENCIA
-            LEFT JOIN FACMUTUO.C_CAD F ON E.AGENTE = F.CONTA
+            INNER JOIN FACMUTUO.CC_TALAO B ON A.COD_TALAO = B.COD_TALAO
+            INNER JOIN FACMUTUO.CC_CADASSOC D ON D.CONTAC = B.CONTAC
+            INNER JOIN FACMUTUO.C_CAD E ON E.CONTA = D.CONTA
         WHERE
             A.COD_SITUACAO = 9
             AND A.COD_MOTDEV IN(11, 12)
-            AND DATA BETWEEN '07/06/2023'
-            AND '04/12/2023'
+            AND A.DATA BETWEEN '07/06/2023' AND '04/12/2023'
             AND TO_CHAR(D.TITULAR) = 'T'
         GROUP BY
             E.CONTA
@@ -217,52 +188,15 @@ FROM
                 FROM
                     (
                         SELECT
-                            TO_CHAR(SYSDATE, 'DD/MM/YYYY') AS DTA_BASE,
                             G.ID_GPSOL AS CDGRUPO,
                             G.DESCR_GRUPO AS DSGRUPO,
-                            G.RESPONSAVEL AS CDTITULAR,
-                            CG.NOME AS NMTITULAR,
-                            C.CONTA AS CDCLIENTE,
-                            C.NOME AS NMCLIENTE,
-                            FACMUTUO.ACHANIVEL(C.NIVEL) AS NIVEL,
-                            DECODE(
-                                G.RESPONSAVEL,
-                                CC.CONTA,
-                                'TITULAR',
-                                DECODE(
-                                    GA.PARENTESCO,
-                                    1,
-                                    'FILHO',
-                                    2,
-                                    'CÔNJUGE',
-                                    3,
-                                    'PAI/MÃE',
-                                    4,
-                                    'GUARDA',
-                                    5,
-                                    'TUTELA',
-                                    6,
-                                    'ENTEADO',
-                                    7,
-                                    'NETO',
-                                    8,
-                                    'IRMÃO',
-                                    9,
-                                    'MARITAL',
-                                    'OUTROS'
-                                )
-                            ) AS PARENTESCO,
                             SUM(
                                 FACMUTUO.EMPREST.PEGASOSALDO(1, GA.CONTA, SYSDATE)
                             ) AS SALDO_EMP,
-                            SUM(NVL(VALOR, 0)) AS SALDO_DESC,
                             SUM(NVL(CC.SALDO, 0)) AS SALDO_CCH
                         FROM
                             FACMUTUO.E_GPSOL G
-                            LEFT JOIN FACMUTUO.C_CAD CG ON G.RESPONSAVEL = CG.CONTA
                             LEFT JOIN FACMUTUO.E_ASSOC_GPSOL GA ON G.ID_GPSOL = GA.ID_GPSOL
-                            LEFT JOIN FACMUTUO.C_CAD C ON GA.CONTA = C.CONTA
-                            LEFT JOIN FACMUTUO.H_CHOPEN CH ON GA.CONTA = CH.CONTA
                             LEFT JOIN (
                                 SELECT
                                     DISTINCT CA.CONTA,
@@ -279,38 +213,7 @@ FROM
                         GROUP BY
                             G.ID_GPSOL,
                             G.DESCR_GRUPO,
-                            G.RESPONSAVEL,
-                            CG.NOME,
-                            C.CONTA,
-                            C.NIVEL,
-                            C.NOME,
-                            DECODE(
-                                G.RESPONSAVEL,
-                                CC.CONTA,
-                                'TITULAR',
-                                DECODE(
-                                    GA.PARENTESCO,
-                                    1,
-                                    'FILHO',
-                                    2,
-                                    'CÔNJUGE',
-                                    3,
-                                    'PAI/MÃE',
-                                    4,
-                                    'GUARDA',
-                                    5,
-                                    'TUTELA',
-                                    6,
-                                    'ENTEADO',
-                                    7,
-                                    'NETO',
-                                    8,
-                                    'IRMÃO',
-                                    9,
-                                    'MARITAL',
-                                    'OUTROS'
-                                )
-                            )
+                            G.RESPONSAVEL
                     )
                 GROUP BY
                     DSGRUPO,
@@ -323,78 +226,67 @@ FROM
     LEFT JOIN (
         SELECT
             CONTAC,
-            CREDITO,
-            DEBITO
+            SUM(CREDITO) AS CREDITO,
+            SUM(DEBITO) AS DEBITO
         FROM
             (
                 SELECT
                     CONTAC,
-                    SUM(CREDITO) AS CREDITO,
-                    SUM(DEBITO) AS DEBITO
+                    SUM(VALOR) AS CREDITO,
+                    0 AS DEBITO
                 FROM
-                    (
-                        SELECT
-                            CONTAC,
-                            SUM(VALOR) AS CREDITO,
-                            0 AS DEBITO
-                        FROM
-                            FACMUTUO.CC_MVOPEN
-                        WHERE
-                            DATA BETWEEN '05/09/2023'
-                            AND '04/12/2023'
-                            AND DC = 'C'
-                        GROUP BY
-                            CONTAC
-                        UNION
-                        ALL
-                        SELECT
-                            CONTAC,
-                            SUM(VALOR) AS CREDITO,
-                            0 AS DEBITO
-                        FROM
-                            FACMUTUO.CC_MVCLOS
-                        WHERE
-                            DATA BETWEEN '05/09/2023'
-                            AND '04/12/2023'
-                            AND DC = 'C'
-                        GROUP BY
-                            CONTAC
-                        UNION
-                        ALL
-                        SELECT
-                            CONTAC,
-                            0 AS CREDITO,
-                            SUM(VALOR) AS DEBITO
-                        FROM
-                            FACMUTUO.CC_MVOPEN
-                        WHERE
-                            DATA BETWEEN '05/09/2023'
-                            AND '04/12/2023'
-                            AND DC = 'D'
-                        GROUP BY
-                            CONTAC
-                        UNION
-                        ALL
-                        SELECT
-                            CONTAC,
-                            0 AS CREDITO,
-                            SUM(VALOR) AS DEBITO
-                        FROM
-                            FACMUTUO.CC_MVCLOS
-                        WHERE
-                            DATA BETWEEN '05/09/2023'
-                            AND '04/12/2023'
-                            AND DC = 'D'
-                        GROUP BY
-                            CONTAC
-                    )
+                    FACMUTUO.CC_MVOPEN
+                WHERE
+                    DATA BETWEEN '05/09/2023'
+                    AND '04/12/2023'
+                    AND DC = 'C'
+                GROUP BY
+                    CONTAC
+                UNION
+                ALL
+                SELECT
+                    CONTAC,
+                    SUM(VALOR) AS CREDITO,
+                    0 AS DEBITO
+                FROM
+                    FACMUTUO.CC_MVCLOS
+                WHERE
+                    DATA BETWEEN '05/09/2023'
+                    AND '04/12/2023'
+                    AND DC = 'C'
+                GROUP BY
+                    CONTAC
+                UNION
+                ALL
+                SELECT
+                    CONTAC,
+                    0 AS CREDITO,
+                    SUM(VALOR) AS DEBITO
+                FROM
+                    FACMUTUO.CC_MVOPEN
+                WHERE
+                    DATA BETWEEN '05/09/2023'
+                    AND '04/12/2023'
+                    AND DC = 'D'
+                GROUP BY
+                    CONTAC
+                UNION
+                ALL
+                SELECT
+                    CONTAC,
+                    0 AS CREDITO,
+                    SUM(VALOR) AS DEBITO
+                FROM
+                    FACMUTUO.CC_MVCLOS
+                WHERE
+                    DATA BETWEEN '05/09/2023'
+                    AND '04/12/2023'
+                    AND DC = 'D'
                 GROUP BY
                     CONTAC
             )
         GROUP BY
-            CONTAC,
-            CREDITO,
-            DEBITO
+            CONTAC
     ) T ON F.CONTAC = T.CONTAC
 WHERE
     C.ASSOCIADO = 'T'
