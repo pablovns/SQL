0e2bea7436645a9685f2cc79ac0f7fd07a45580f
pablovns <viniciuspablo962@gmail.com>
2024-02-20 10:51:28 -0300

    ADicionar parametro adicional do nome da funcao

--- conversor.py
+++ conversor.py
@@ -1,13 +1,13 @@
 import os
 import sys
 
-def converter_sql_cs(arquivo_sql, arquivo_cs):
+def converter_sql_cs(arquivo_sql, arquivo_cs, nome_funcao=None):
     conteudo_sql = ""
     with open(arquivo_sql, 'r') as arquivo_entrada:
         for linha in arquivo_entrada:
-            conteudo_sql += f"stbSelecao.AppendLine(\"{linha.strip()}\");\n\t"
+            conteudo_sql += f"stbSql.AppendLine(\"{linha.strip()}\");\n\t"
 
-    conteudo_completo = f"""public string RetornaSQL()
+    conteudo_completo = f"""public string RetornaSQL{nome_funcao if nome_funcao else None}()
 {{
     StringBuilder stbSql = new StringBuilder();
 
@@ -24,12 +24,18 @@ def converter_sql_cs(arquivo_sql, arquivo_cs):
         arquivo_saida.write(conteudo_completo)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Uso: python conversor.py arquivo_sql")
+    if len(sys.argv) < 2 or len(sys.argv) > 3:
+        print("Uso: python conversor.py arquivo_sql [nome_funcao]")
         sys.exit(1)
     
     arquivo_sql = sys.argv[1]
     arquivo_cs = f"{arquivo_sql[:-4]}.cs"  # Substitui a extensão do arquivo por .cs e salva na pasta cs/
 
-    converter_sql_cs(arquivo_sql, arquivo_cs)
+    if len(sys.argv) == 3:
+        nome_funcao = sys.argv[2]
+        converter_sql_cs(arquivo_sql, arquivo_cs, nome_funcao)
+    else:
+        converter_sql_cs(arquivo_sql, arquivo_cs)
+
     print(f"Arquivo traduzido salvo como '{arquivo_cs}'")
+
--- cs/declaracao_residencia.cs
+++ cs/declaracao_residencia.cs
@@ -1,27 +1,27 @@
-public string RetornaSQL()
+public string RetornaSQLDeclaracaoResidencia()
 {
     StringBuilder stbSql = new StringBuilder();
 
-    stbSelecao.AppendLine("SELECT");
-	stbSelecao.AppendLine("C.NOME ASSOCIADO,");
-	stbSelecao.AppendLine("COALESCE(C.CPF, C.CGC) CPF_CPNJ,");
-	stbSelecao.AppendLine("C.CI RG,");
-	stbSelecao.AppendLine("C.ORG_EXP,");
-	stbSelecao.AppendLine("C.NACIONALIDADE,");
-	stbSelecao.AppendLine("C.ENDERECO,");
-	stbSelecao.AppendLine("C.END_NUM NUMERO,");
-	stbSelecao.AppendLine("C.BAIRRO,");
-	stbSelecao.AppendLine("D.NOME CIDADE,");
-	stbSelecao.AppendLine("D.UF ESTADO,");
-	stbSelecao.AppendLine("C.CEP");
-	stbSelecao.AppendLine("FROM");
-	stbSelecao.AppendLine("FACMUTUO.C_CAD C");
-	stbSelecao.AppendLine("LEFT JOIN FACMUTUO.C_CIDADE D ON C.COD_CID = D.COD_CID");
-	stbSelecao.AppendLine("WHERE");
-	stbSelecao.AppendLine("C.ASSOCIADO = 'T'");
-	stbSelecao.AppendLine("AND C.SITUACAO = 'Normal'");
-	stbSelecao.AppendLine("ORDER BY");
-	stbSelecao.AppendLine("ASSOCIADO");
+    stbSql.AppendLine("SELECT");
+	stbSql.AppendLine("C.NOME ASSOCIADO,");
+	stbSql.AppendLine("COALESCE(C.CPF, C.CGC) CPF_CPNJ,");
+	stbSql.AppendLine("C.CI RG,");
+	stbSql.AppendLine("C.ORG_EXP,");
+	stbSql.AppendLine("C.NACIONALIDADE,");
+	stbSql.AppendLine("C.ENDERECO,");
+	stbSql.AppendLine("C.END_NUM NUMERO,");
+	stbSql.AppendLine("C.BAIRRO,");
+	stbSql.AppendLine("D.NOME CIDADE,");
+	stbSql.AppendLine("D.UF ESTADO,");
+	stbSql.AppendLine("C.CEP");
+	stbSql.AppendLine("FROM");
+	stbSql.AppendLine("FACMUTUO.C_CAD C");
+	stbSql.AppendLine("LEFT JOIN FACMUTUO.C_CIDADE D ON C.COD_CID = D.COD_CID");
+	stbSql.AppendLine("WHERE");
+	stbSql.AppendLine("C.ASSOCIADO = 'T'");
+	stbSql.AppendLine("AND C.SITUACAO = 'Normal'");
+	stbSql.AppendLine("ORDER BY");
+	stbSql.AppendLine("ASSOCIADO");
 	
     return stbSql.ToString();
 }
